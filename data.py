dcd94ac5b67d7efa2977d456aebfb69a6c9f950b
jasonli8408 <jasonli8408@gmail.com>
2023-04-02 17:32:46 -0400

    add gan pretrain

--- data.py
+++ data.py
@@ -25,3 +25,27 @@ class COVIDDataset(Dataset):
         if self.transform is not None:
             image = self.transform(image)
         return image, label
+    
+class GANDataset(Dataset):
+    """Two folders: corona and normal, each consists of 500 images"""
+    def __init__(self, data_dir, transform=None):
+        self.data = []
+        self.transform = transform
+        self.map = {'corona': 1, 'normal': 0}
+        
+        # iterate through corona and normal folders
+        for folder in os.listdir(data_dir):
+            folder_path = os.path.join(data_dir, folder)
+            for filename in os.listdir(folder_path):
+                image_path = os.path.join(folder_path, filename)
+                self.data.append((image_path, self.map[folder]))
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        image_path, label = self.data[index]
+        image = Image.open(image_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
